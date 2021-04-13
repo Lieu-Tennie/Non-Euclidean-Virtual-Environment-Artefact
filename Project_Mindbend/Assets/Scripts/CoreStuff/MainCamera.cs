19d624046b36260b09d9a5c4ec72ff937466f686
Oliver <ot218255@falmouth.ac.uk>
2021-04-13 10:56:43 +0100

    + Added 'MindBend' logo. + Changed level 1's layout + Created a basic title screen. + Changed Player script.

---
+++ Project_Mindbend/Assets/Scripts/CoreStuff/MainCamera.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCamera : MonoBehaviour
+{
+    Portal[] portals;
+
+    void Awake()
+    {
+        portals = FindObjectsOfType<Portal>();
+    }
+}
---
+++ Project_Mindbend/Assets/Scripts/CoreStuff/Portal.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    public Portal linkedPortal;
+    public MeshRenderer screen;
+    Camera playerCam;
+    Camera portalCam;
+    RenderTexture viewTexture;
+
+    void Awake()
+    {
+        playerCam = Camera.main;
+        portalCam = GetComponentInChildren<Camera>();
+        screen.enabled = true;
+    }
+
+    void CreateViewTexture()
+    {
+        if (viewTexture == null || viewTexture.width != Screen.width || viewTexture.height != Screen.height)
+        {
+            if (viewTexture != null)
+            {
+                viewTexture.Release();
+            }
+            viewTexture = new RenderTexture(Screen.width, Screen.height, 0);
+            // Renders the view from the portal camera to the view of the texture
+            portalCam.targetTexture = viewTexture; 
+            //Displays the view texture of the linked portal onto the screen
+            linkedPortal.screen.material.SetTexture("_MainTex", viewTexture);
+        }
+    }
+
+    // This is called before the player camera has been rendered
+    public void Render() 
+    {
+        screen.enabled = false;
+        CreateViewTexture();
+
+        //Make portal cam position and rotation the same relative to this portal as the player cam is relative to the linked portal
+        var m = transform.localToWorldMatrix * linkedPortal.transform.worldToLocalMatrix * playerCam.transform.localToWorldMatrix;
+        portalCam.transform.SetPositionAndRotation(m.GetColumn(3), m.rotation);
+
+        //Render the camera
+        portalCam.Render();
+
+        screen.enabled = true;
+    }
+}
